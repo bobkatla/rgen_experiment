293663a19e231414c6bc6079b6491ae33d271997
Duc Minh La <dminh.bob.la@gmail.com>
2025-09-12 14:21:11 +1000

    minors changes and added the test with urc

---
+++ scripts/02.1_test_urc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+uv run rgen train \
+  --data-dir data/cifar10/train \
+  --out-dir output/cifar10_unet_eps_cfg_urc \
+  --batch-size 256 --lr 2e-4 --timesteps 4000 \
+  --iters 200000 --ema 0.9999 --amp --seed 0 \
+  --label-drop-prob 0.1 --num-workers 3 \
+  --use-urc --urc-weight 0.1 --urc-alpha 0.05 --urc-dy 128
