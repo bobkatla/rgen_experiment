57f054ef7d4518dc18b4b368f8f1aa42c6ce149b
Duc Minh La <dminh.bob.la@gmail.com>
2025-09-16 20:48:35 +1000

    minors

--- scripts/m3/01.2_train_cfg_urc_n_sample.sh
+++ scripts/m3/01.2_train_cfg_urc_n_sample.sh
@@ -18,3 +18,13 @@ rgen train \
   --iters 200000 --ema 0.9999 --amp --seed 0 \
   --label-drop-prob 0.1 --num-workers 3 \
   --use-urc --urc-weight 0.2 --urc-alpha 0.20 --urc-dy 128
+
+CKPT=output/updated2_cifar10_unet_eps_cfg_urc/ckpt_ema_0200000.pt  # <- pick an EMA checkpoint
+
+rgen sample \
+  --ckpt "$CKPT" \
+  --out-dir samples/urc_ddpm4000 \
+  --sample-type ddpm --guidance --w 3 --steps 4000 \
+  --n-samples 50000
+
+rgen eval-fid --gen-dir samples/urc_ddpm4000 --reference folder --ref-dir data/cifar10/test
